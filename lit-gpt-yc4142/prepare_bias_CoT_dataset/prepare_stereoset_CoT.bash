69504e6e4dcd0628f0757d8f9370d579a660b1b6
Yudu Chen <yuduc2@yudus-mbp-514.myfiosgateway.com>
2023-11-26 23:12:56 -0500

    added CoT dataset on bias

---
+++ lit-gpt-yc4142/prepare_bias_CoT_dataset/prepare_stereoset_CoT.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir data
+mkdir data/stereoset_CoT
+
+cd ..
+
+python3 scripts/download.py\
+  --repo_id togethercomputer/RedPajama-INCITE-Instruct-3B-v1
+
+python3 scripts/convert_hf_checkpoint.py\
+  --checkpoint_dir checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1
+
+#python3 prepare_bias_CoT_dataset/load_stereoset_CoT.py \
+#  --api_key "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" \
+#  --destination_path "prepare_bias_CoT_dataset/data/stereoset_CoT" \
+#  --out_file_name "bias_CoT_explanation"
+
+python3 prepare_bias_CoT_dataset/load_stereoset_CoT_multi_thread.py \
+  --api_key "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" \
+  --destination_path "prepare_bias_CoT_dataset/data/stereoset_CoT" \
+  --out_file_name "bias_CoT_explanation_multi_thread"
+
+#python3 prepare_stereoset_CoT.py \
+#  --destination_path "data/stereoset_CoT" \
+#  --checkpoint_dir "checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1" \
+#  --data_file_name "bias_CoT_explanation_total.json"
+
