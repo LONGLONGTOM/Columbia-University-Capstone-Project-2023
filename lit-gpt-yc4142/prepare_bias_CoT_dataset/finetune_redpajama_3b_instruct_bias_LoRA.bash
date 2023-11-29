5790c1b1dadac797710e3290a38eaa6f96fd63cf
Yudu Chen <yuduc2@yudus-mbp-514.myfiosgateway.com>
2023-11-29 18:51:23 -0500

    added bash file for fine tune on bias dataset

---
+++ lit-gpt-yc4142/prepare_bias_CoT_dataset/finetune_redpajama_3b_instruct_bias_LoRA.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir prepare_bias_CoT_dataset/out
+
+python3 finetune/lora.py \
+  --checkpoint_dir "checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1" \
+  --data_dir "prepare_bias_CoT_dataset/data" \
+  --out_dir "prepare_bias_CoT_dataset/out/lora_weights_logiqa/RedPajama-INCITE-Instruct-3B-v1/" \
+
+
+python3 scripts/merge_lora.py \
+  --checkpoint_dir "checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1"  \
+  --lora_path "prepare_bias_CoT_dataset/out/lora_weights_logiqa/RedPajama-INCITE-Instruct-3B-v1/lit_model_lora_finetuned.pth" \
+  --out_dir "prepare_bias_CoT_dataset/out/lora_merged_logiqa/RedPajama-INCITE-Instruct-3B-v1/"\
+
+cp checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1/*.json \
+prepare_bias_CoT_dataset/out/lora_merged_logiqa/RedPajama-INCITE-Instruct-3B-v1/
