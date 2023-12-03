d4fede7cb82f35e22321fc78421c35d564e60e32
Yudu Chen <yc4142@columbia.edu>
2023-12-03 05:39:34 +0000

    some change in fine tuning script

--- lit-gpt-yc4142/prepare_bias_CoT_dataset/finetune_redpajama_3b_instruct_bias_LoRA_CoT.bash
+++ lit-gpt-yc4142/prepare_bias_CoT_dataset/finetune_redpajama_3b_instruct_bias_LoRA_CoT.bash
@@ -5,7 +5,8 @@ mkdir prepare_bias_CoT_dataset/out/CoT
 python3 prepare_bias_CoT_dataset/prepare_stereoset_data.py \
   --destination_path "prepare_bias_CoT_dataset/data/CoT" \
   --checkpoint_dir "checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1" \
-  --data_file_name "bias_CoT_reasoning_scrambled_train.json"
+  --data_file_name "bias_CoT_reasoning_scrambled_train.json"\
+  --max_seq_length 512
 
 python3 finetune/lora.py \
   --checkpoint_dir "checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1" \
@@ -14,13 +15,14 @@ python3 finetune/lora.py \
   --precision "32-true"\
   --quantize "bnb.nf4"
 
-mkdir prepare_bias_CoT_dataset/out/CoT/lora_merged_metaeval
-mkdir prepare_bias_CoT_dataset/out/CoT/lora_merged_metaeval/RedPajama-INCITE-Instruct-3B-v1
+mkdir prepare_bias_CoT_dataset/out/CoT/lora_merged_stereoset
+mkdir prepare_bias_CoT_dataset/out/CoT/lora_merged_stereoset/RedPajama-INCITE-Instruct-3B-v1
+
+cp checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1/*.json \
+prepare_bias_CoT_dataset/out/CoT/lora_merged_stereoset/RedPajama-INCITE-Instruct-3B-v1/
 
 python3 scripts/merge_lora.py \
   --checkpoint_dir "checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1"  \
   --lora_path "prepare_bias_CoT_dataset/out/CoT/lora_weights_stereoset/RedPajama-INCITE-Instruct-3B-v1/lit_model_lora_finetuned.pth" \
   --out_dir "prepare_bias_CoT_dataset/out/CoT/lora_merged_stereoset/RedPajama-INCITE-Instruct-3B-v1/"\
 
-cp checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1/*.json \
-prepare_bias_CoT_dataset/out/CoT/lora_merged_stereoset/RedPajama-INCITE-Instruct-3B-v1/
--- lit-gpt-yc4142/prepare_ethics_CoT_dataset/finetune_redpajama_3b_instruct_ethics_LoRA_CoT.bash
+++ lit-gpt-yc4142/prepare_ethics_CoT_dataset/finetune_redpajama_3b_instruct_ethics_LoRA_CoT.bash
@@ -7,6 +7,7 @@ python3 prepare_ethics_CoT_dataset/prepare_metaeval_data.py \
   --destination_path "prepare_ethics_CoT_dataset/data/CoT" \
   --checkpoint_dir "checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1" \
   --data_file_name "ethics_CoT_reasoning_scrambled.json" \
+  --max_seq_length 512
 
 python3 finetune/lora.py \
   --checkpoint_dir "checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1" \
@@ -18,12 +19,12 @@ python3 finetune/lora.py \
 mkdir prepare_ethics_CoT_dataset/out/CoT/lora_merged_metaeval
 mkdir prepare_ethics_CoT_dataset/out/CoT/lora_merged_metaeval/RedPajama-INCITE-Instruct-3B-v1
 
+cp checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1/*.json \
+prepare_ethics_CoT_dataset/out/CoT/lora_merged_metaeval/RedPajama-INCITE-Instruct-3B-v1/
+
 python3 scripts/merge_lora.py \
   --checkpoint_dir "checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1"  \
   --lora_path "prepare_ethics_CoT_dataset/out/CoT/lora_weights_metaeval/RedPajama-INCITE-Instruct-3B-v1/lit_model_lora_finetuned.pth" \
   --out_dir "prepare_ethics_CoT_dataset/out/CoT/lora_merged_metaeval/RedPajama-INCITE-Instruct-3B-v1/"\
 
-
-cp checkpoints/togethercomputer/RedPajama-INCITE-Instruct-3B-v1/*.json \
-prepare_ethics_CoT_dataset/out/CoT/lora_merged_metaeval/RedPajama-INCITE-Instruct-3B-v1/
 
